26cf746386130424fdf07cd8ffc22685a50a7760
github-ci <you@example.com>
2025-12-19 17:02:54 +0800

    upgrade

--- src/DependencyInjection/HotelCardDeliveryExtension.php
+++ src/DependencyInjection/HotelCardDeliveryExtension.php
@@ -4,7 +4,7 @@ namespace Tourze\HotelCardDeliveryBundle\DependencyInjection;
 
 use Tourze\SymfonyDependencyServiceLoader\AutoExtension;
 
-class HotelCardDeliveryExtension extends AutoExtension
+final class HotelCardDeliveryExtension extends AutoExtension
 {
     protected function getConfigDir(): string
     {
